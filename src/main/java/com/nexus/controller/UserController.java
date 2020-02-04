99ed70360483ff8a9dcaec1b6ca7bae72d4e10ba
loveleen <loveleen.narang@gmail.com>
2020-02-04 16:27:54 -0500

    Closes #17

--- src/main/java/com/nexus/controller/UserController.java
+++ src/main/java/com/nexus/controller/UserController.java
@@ -3,6 +3,7 @@ package com.nexus.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nexus.persistence.entity.User;
@@ -11,36 +12,43 @@ import com.nexus.service.UserService;
 @RestController
 public class UserController {
 
-	
 	private UserService userService;
 
 	public UserController(UserService userService) {
 		this.userService = userService;
 	}
-	
-	
+
 	@GetMapping("/users")
-	public List<User> getAllUsers(){
-		return userService.getAllUsers();
+	public List<User> getAllUsers(@RequestParam(required = false) Boolean active,
+			@RequestParam(required = false) String eyeColor, @RequestParam(required = false) Double balance,
+			@RequestParam(required = false) Integer age, @RequestParam(required = false) String gender,
+			@RequestParam(required = false) Integer zipCode) {
+		if (balance == null)
+			balance = Double.valueOf(0);
+		if (age == null)
+			age = Integer.valueOf(0);
+		if (zipCode == null)
+			zipCode = Integer.valueOf(0);
+		return userService.getAllUsers(active, eyeColor, balance, age.intValue(), zipCode.intValue(), gender);
 	}
-	
+
 	@GetMapping("/users/highbalance")
-	public List<User> getHighBalanceUsers(){
+	public List<User> getHighBalanceUsers() {
 		return userService.getHighBalanceUsers();
 	}
-	
+
 	@GetMapping("/users/active")
-	public List<User> getActiveUsers(){
+	public List<User> getActiveUsers() {
 		return userService.getActiveUsers();
 	}
-	
+
 	@GetMapping("/users/inactive")
-	public List<User> getInActiveUsers(){
+	public List<User> getInActiveUsers() {
 		return userService.getInActiveUsers();
 	}
-	
+
 	@GetMapping("/users/females")
-	public List<User> getFemaleUsers(){
+	public List<User> getFemaleUsers() {
 		return userService.getFemaleUsers();
 	}
 }
--- src/main/java/com/nexus/service/UserService.java
+++ src/main/java/com/nexus/service/UserService.java
@@ -64,4 +64,16 @@ public class UserService {
 				.filter(user -> user.getGender().equalsIgnoreCase("female"))
 				.collect(Collectors.toList());
 	}
+
+	public List<User> getAllUsers(Boolean active, String eyeColor, Double balance, int age, int zipCode, String gender) {
+		return userRepo
+				.findAll()
+				.stream()
+				.filter(user -> active==null ||user.getIsActive()==active.booleanValue())
+				.filter(user -> eyeColor == null ||user.getEyeColor().equalsIgnoreCase(eyeColor))
+				.filter(user -> user.getBalance() >= balance.doubleValue())
+				.filter(user -> age ==0 || user.getAge() == age)
+				.filter(user -> zipCode ==0 || user.getAddress().getZip() == zipCode)
+				.collect(Collectors.toList());
+	}
 }
