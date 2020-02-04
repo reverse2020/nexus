3161443435c10bfea29c08d07da944df315d7d26
loveleen <loveleen.narang@gmail.com>
2020-02-04 12:48:26 -0500

    Closes #16

--- src/main/java/com/nexus/controller/UserController.java
+++ src/main/java/com/nexus/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.nexus.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nexus.persistence.entity.User;
 import com.nexus.service.UserService;
 
 @RestController
@@ -15,4 +19,8 @@ public class UserController {
 	}
 	
 	
+	@GetMapping("/users")
+	public List<User> getAllUsers(){
+		return userService.getAllUsers();
+	}
 }
