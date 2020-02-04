35d2930fc5728c466422e074e8bf94ec383ec6b0
loveleen <loveleen.narang@gmail.com>
2020-02-04 09:41:38 -0500

    Closes #14

--- src/main/java/com/nexus/bootstrap/DataLoader.java
+++ src/main/java/com/nexus/bootstrap/DataLoader.java
@@ -2,26 +2,38 @@ package com.nexus.bootstrap;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nexus.persistence.entity.User;
 import com.nexus.persistence.repo.UserRepo;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class DataLoader {
 
-	private ObjectMapper objectMapper = new ObjectMapper();
-	
+
 	@Autowired
 	private UserRepo userRepo;
 
+	@PostConstruct
+	private void loadUsers() {
+		ObjectMapper mapper = new ObjectMapper();
+		TypeReference<List<User>> typeReference = new TypeReference<List<User>>() {};
+		InputStream inputStream = TypeReference.class.getResourceAsStream("/bootstrap.json");
+		try {
+			List<User> users = mapper.readValue(inputStream, typeReference);
+			userRepo.saveAll(users);
+			System.out.println("Users Saved!");
+		} catch (IOException e) {
+			System.out.println("Unable to save users: " + e.getMessage());
+		}
+
+	}
 }
